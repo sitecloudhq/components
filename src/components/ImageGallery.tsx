091c248c4870ae28b5b515bba7c86bb49479c39d
Juan Fernández <juan@acyclic.co>
2021-10-20 09:13:27 +0200

    feat(ImageGallery): grid and irregular gallery types

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,31 +1,58 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { ImageAlt as Icon } from 'styled-icons/boxicons-regular';
 
 import { PropTypes, EditorTypes } from '../types';
 import { styleProps } from '../utils';
 import { Component } from '../Component';
 
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+const gridGallery = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 51vw;
+
+  ${Image}:nth-child(1n) {
+    grid-column: 1;
+  }
+  ${Image}:nth-child(2n) {
+    grid-column: 2;
+  }
+  ${styleProps({
+    gap: 'grid-gap'
+  })};
+`;
+
+const irregularGallery = css`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-auto-rows: 10vw;
+  ${Image}:nth-child(odd) {
+    grid-row: span 4;
+  }
+  ${Image}:nth-child(even) {
+    grid-row: span 3;
+  }
+  ${styleProps({
+    gap: 'grid-gap'
+  })};
+`;
+
 const Container = styled.div`
   ${(props: any) => props.styled}
-  display: flex;
-  flex: 1;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
+  ${(props: any) =>
+    props.gallery == 'irregular' ? irregularGallery : gridGallery};
   ${styleProps({
     width: 'width',
     height: 'height',
     padding: 'padding',
     margin: 'margin'
   })}
-`;
-
-const Image = styled.img`
-  margin: 10px;
-  width: 200px;
-  height: 150px;
-  object-fit: cover;
 `;
 
 const ImageGallery: Component<{ images: { sources: string[] } }> = ({
@@ -47,6 +74,20 @@ ImageGallery.props = {
     default: [],
     editor: EditorTypes.ImageGallery
   },
+  gap: {
+    type: PropTypes.UnitValue,
+    default: '10px',
+    editor: {
+      type: EditorTypes.UnitValue,
+      options: [PropTypes.UnitValue.px]
+    }
+  },
+  gallery: {
+    type: PropTypes.Array,
+    default: ['grid', 'irregular'],
+    editor: EditorTypes.Combo
+  },
+
   width: {
     type: PropTypes.UnitValue,
     default: 'auto',
--- stories/8.ImageGallery.stories.js
+++ stories/8.ImageGallery.stories.js
@@ -9,14 +9,18 @@ export default {
 
 export const image_gallery = () => (
   <ImageGallery
-    padding={{ top: 25, left: 0, bottom: 0, right: 0 }}
+    padding={{ top: 0, left: 0, bottom: 0, right: 0 }}
+    gap="15px"
+    gallery="irregular"
     images={{
       sources: [
-        'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyMzE1Mzk0fHxlbnwwfHx8fA%3D%3D&w=1000&q=80',
         'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg',
         'https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg',
         'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXwyMzE1Mzk0fHxlbnwwfHx8fA%3D%3D&w=1000&q=80',
         'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554__340.jpg',
+        'https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg',
+        'https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg',
+        'https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg',
         'https://i.cbc.ca/1.5256404.1566499707!/fileImage/httpImage/cat-behaviour.jpg'
       ]
     }}
