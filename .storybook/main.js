5076b0cb9df0383a1a9d38d43a9ed7a674b8ef1d
Juan Fernández <juan@acyclic.co>
2021-11-05 22:12:03 +0100

    chore: removed storybook dep

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,7 @@
 module.exports = {
-  stories: ["../stories/**/*.stories.js"],
-  addons: [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-knobs/register"
-  ],
-  webpackFinal: async config => {
+  stories: ['../stories/**/*.stories.js'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  webpackFinal: async (config) => {
     // do mutation to the config
 
     return config;
