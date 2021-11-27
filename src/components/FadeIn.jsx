f0e27563b56f8922f15bc54927caadb6dfa5a2b8
Juan Fernández <juan@acyclic.co>
2021-11-27 22:09:12 +0100

    fix: fadein effect shouldn't modify position

--- src/components/FadeIn.jsx
+++ src/components/FadeIn.jsx
@@ -5,28 +5,28 @@ import styled, { css } from 'styled-components';
 
 const Container = styled.div`
   opacity: 0;
-  transform: translateY(20vh);
-  visibility: hidden;
-  transition: opacity 0.6s ease-out, transform 0.6s ease-out;
-  will-change: opacity, visibility;
+  transition: opacity 0.5s ease-out;
 
   ${(props) =>
     props.visible &&
     css`
       opacity: 1;
       transform: none;
-      visibility: visible;
     `}
 `;
 
 const FadeInSection = ({ children }) => {
-  const [isVisible, setVisible] = React.useState(true);
+  const [isVisible, setVisible] = React.useState(false);
   const domRef = React.useRef();
 
   useEffect(() => {
     const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => setVisible(entry.isIntersecting));
+      entries.forEach((entry) => {
+        setVisible(entry.isIntersecting);
+      });
+      observer.unobserve(domRef.current);
     });
+
     observer.observe(domRef.current);
     return () => observer.unobserve(domRef.current);
   }, []);
