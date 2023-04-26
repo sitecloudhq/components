8dc3bad318a835fcdce0cc8f34ba4acbe01a987e
Juan Fernández <juan@acyclic.co>
2023-04-26 19:01:31 +0200

    chore: deps and storybook configuration updated

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,37 @@
 const path = require('path');
-
 module.exports = {
   stories: ['../src/stories/*.stories.js'],
+
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+
   webpackFinal: async (config) => {
     config.resolve.modules = [
       ...(config.resolve.modules || []),
       path.resolve(__dirname, '..')
     ];
-    config.resolve.extensions.push('.ts', '.tsx');
+
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      ]
+    });
+
+    config.resolve.extensions.push('.ts', '.tsx', '.js', '.jsx');
 
     return config;
+  },
+  core: {
+    builder: 'webpack5'
   }
 };
